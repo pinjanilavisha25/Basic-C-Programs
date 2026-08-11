02e74bb5e9dd7dc47168414548050981edb05a69
Lavisha Pinjani <pinjanilavisha@gmail.com>
2026-08-12 00:57:29 +0530

    Program8

---
+++ fahrenheit_celsius.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float celsius,fahrenheit;
+    cout<<"Enter the temperature in celsius:";
+    cin>>celsius;
+    fahrenheit=(celsius*9/5)+32;
+    cout<<"The temperature in fahrenheit is:"<<fahrenheit<<endl;
+    return 0;
+}
