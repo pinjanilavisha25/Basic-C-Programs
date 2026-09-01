541c8ace5ba3f6e2a77c0427e4ab728beaf95d11
Lavisha Pinjani <pinjanilavisha@gmail.com>
2026-09-01 22:08:31 +0530

    Merge two arrays

---
+++ merge_two_arrays.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr1[]={1,2,3};
+    int arr2[]={4,5,6,7};
+    int size1=sizeof(arr1)/sizeof(arr1[0]);
+    int size2=sizeof(arr2)/sizeof(arr2[0]);
+    int size3=size1+size2;
+    int arr3[size3]; 
+    for(int i=0;i<size1;i++){
+       arr3[i]=arr1[i];
+    }
+    for(int i=0;i<size2;i++){
+       arr3[size1+i]=arr2[i];
+    }
+
+    cout<<"Merged Array:";
+    for(int i=0;i<size3;i++){
+        cout<<arr3[i]<<endl;
+    }
+
+    return 0;
+
+
+
+
+
+
+
+}
